f319286fc89e258155aaeb761742cc9c8276999c
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-13 19:38:20 -0400

    Create Logo_4.jsx
    Adding file

---
+++ front_end/front-end/src/components/Logo_4.jsx
@@ -0,0 +1,15 @@
+// Logo.jsx
+import React from 'react';
+import styled from 'styled-components';
+import logo from '../assets/logo_2.svg';
+
+const StyledLogo = styled.img`
+  top: 50px;
+  left: 63px;
+  width: 396px;
+  height: 90px;
+`;
+
+const Logo = () => <StyledLogo src={logo} alt="Logo" />;
+
+export default Logo;
