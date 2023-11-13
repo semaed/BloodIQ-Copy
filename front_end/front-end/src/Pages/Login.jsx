eb693ae828aea56ce6fab6aac12cbd6327eb4586
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-13 19:28:45 -0400

    Create Login.jsx
    Adding file

---
+++ front_end/front-end/src/Pages/Login.jsx
@@ -0,0 +1,17 @@
+// Login.jsx
+import Logo from '../components/Logo';
+import React from 'react';
+import "../App.css"
+import LoginForm from '../components/Forms/LoginForm'; 
+
+function Login() {
+  return (
+    <>
+      <div id="rectangle"></div>
+      <Logo />
+      <LoginForm />
+    </>
+  );
+}
+
+export default Login;
