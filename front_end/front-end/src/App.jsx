6652493dcc5acb18b6cf034fbde24dcc7a1ef00a
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-13 19:48:08 -0400

    Create App.jsx
    Adding file

---
+++ front_end/front-end/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {AppRoutes} from './routers/routes';
+import {LoadingResults} from './Pages/LoadingResults';
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
