18aff1f2fdb124b360263e4c1005abd5aefd63c2
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 19:57:57 -0400

    Update testFile.py
    Adding comments to the code

--- testFile.py
+++ testFile.py
@@ -1,6 +1,8 @@
-#!/bin/bash/python3
-
+#!/usr/bin/env python3  # Corrected shebang line for Python 3
 
 class Parent:
+    # Class variable 'flag', shared by all instances of this class, initialized to 0
     flag = 0
+
+    # Class variable 'originFile', shared by all instances, initialized with a file name
     originFile = "resultadopdf-4.pdf"
