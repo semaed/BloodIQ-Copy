3189432746716ecdf4b6e1bc4232b993d554eec7
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 19:59:51 -0400

    Update start.py
    Adding comments to the code file

--- start.py
+++ start.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
-import Flask
-# from numberExtract import function
+#!/usr/bin/python3  # Shebang line specifying the Python 3 interpreter
 
-app = Flask(__name__)
+import Flask  # Imports the Flask module
 
+# from numberExtract import function  # Commented out import statement (possibly for a specific function from a module)
 
-@app.route('/')
-def userResponse():
-    return  # function
+app = Flask(__name__)  # Creates a Flask application instance
+
+@app.route('/')  # Decorator that defines the route for the root URL ('/')
+def userResponse():  # Defines a function to handle requests to the root URL
+    return  # Here should be the response or call to a function (currently just a return statement with no value)
