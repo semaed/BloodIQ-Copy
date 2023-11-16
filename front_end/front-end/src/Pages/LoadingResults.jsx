d736ee45a0670e4088879db212eb33895bacbaf5
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:30:12 -0400

    Update LoadingResults.jsx
    Adding comments to the code

--- front_end/front-end/src/Pages/LoadingResults.jsx
+++ front_end/front-end/src/Pages/LoadingResults.jsx
@@ -1,32 +1,56 @@
-import styled from "styled-components";
-import "../index.css"
-import Logo from "../components/Logo_2";
-import ClockLoader from "react-spinners/ClockLoader";
+import styled from "styled-components";  // Importing styled-components for custom styled components
+import "../index.css"  // Importing the main CSS file for global styles
+import Logo from "../components/Logo_2";  // Importing the Logo component
+import ClockLoader from "react-spinners/ClockLoader";  // Importing ClockLoader spinner from react-spinners
 
+// Define the LoadingResults functional component
 export function LoadingResults() {
-    return ( <Container>
-        <div style={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
-            <Logo/>
-        </div>
-        <h1 style={{color: '#fff', fontFamily: 'Inter, sans-serif', fontSize: '2rem', top: '200px', marginTop: '2rem'}}>Loading Results</h1>
-        <ClockLoader color={"#FF3131"} size={150} />
-        <h3 style={{ color: '#fff', fontFamily: 'Inter, sans-serif', fontSize: '16px', marginTop: '2rem'}}>Exploring your blood digitally... <br />
-         the information is on its way.</h3>
-    </Container>);
+    return (
+        <Container> {/* Using the Container styled component for layout */}
+            <div style={{ display: 'flex', justifyContent: 'center', width: '100%'}}>
+                <Logo/> {/* Rendering the Logo component */}
+            </div>
+            <h1 
+                style={{ 
+                    color: '#fff', 
+                    fontFamily: 'Inter, sans-serif', 
+                    fontSize: '2rem', 
+                    top: '200px', 
+                    marginTop: '2rem'
+                }}
+            >
+                Loading Results
+            </h1> {/* Heading with custom styles */}
+            <ClockLoader color={"#FF3131"} size={150} /> {/* ClockLoader spinner component */}
+            <h3 
+                style={{ 
+                    color: '#fff', 
+                    fontFamily: 'Inter, sans-serif', 
+                    fontSize: '16px', 
+                    marginTop: '2rem'
+                }}
+            >
+                Exploring your blood digitally... <br />
+                the information is on its way.
+            </h3> {/* Subheading with custom styles */}
+        </Container>
+    );
 }
 
-const Container =styled.div`
-margintop: 0;
-position: absolute;
-height: 100vh;
-width: 100%;
-background-color: #2E9DDD;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-textcolor: white;
-font-family: 'Inter', sans-serif;
-padding: 5px 0;
+// Styled-component for Container
+const Container = styled.div`
+    margin-top: 0;
+    position: absolute;
+    height: 100vh;  // 100% of the viewport height
+    width: 100%;  // 100% width of the parent element
+    background-color: #2E9DDD;  // Background color
+    display: flex;  // Using flexbox for layout
+    flex-direction: column;  // Children elements in column direction
+    justify-content: center;  // Centering content vertically
+    align-items: center;  // Centering content horizontally
+    text-color: white;  // White text color
+    font-family: 'Inter', sans-serif;  // Font family
+    padding: 5px 0;  // Padding
 `;
-export default LoadingResults;
+
+export default LoadingResults;  // Export the LoadingResults component for use in other parts of the application
