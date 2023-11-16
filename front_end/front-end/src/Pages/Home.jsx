7205799e531e8fac1c9811fe6128839c9412c78a
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:23:33 -0400

    Update Home.jsx
    Adding comments to the code

--- front_end/front-end/src/Pages/Home.jsx
+++ front_end/front-end/src/Pages/Home.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
-import Logo from '../components/Logo_3';
+import React from 'react';  // Importing React to enable JSX and functional components
+import { Link } from 'react-router-dom';  // Importing Link from react-router-dom for navigation
+import Logo from '../components/Logo_3';  // Importing the Logo component
 
+// Define the Home functional component
 const Home = () => {
   return (
+    // React Fragment to return multiple elements
     <>
+      {/* Main container div */}
       <div id="rectangle">
-        <Logo />
-        <h2 style={{ fontSize: '48px', position: 'absolute', color: '#fff', top: '241px', left: '86px'}}>Your Hematological <br /> Health Advisor!</h2>
-        <p style={{ fontSize: '24px', color: '#fff', position: 'absolute', left: '86px', top: '420px' }}>
+        <Logo />  {/* Rendering the Logo component */}
+        <h2 
+          style={{ 
+            fontSize: '48px', 
+            position: 'absolute', 
+            color: '#fff', 
+            top: '241px', 
+            left: '86px'
+          }}
+        >
+          Your Hematological <br /> Health Advisor!
+        </h2> {/* Heading with custom styles */}
+        <p 
+          style={{ 
+            fontSize: '24px', 
+            color: '#fff', 
+            position: 'absolute', 
+            left: '86px', 
+            top: '420px' 
+          }}
+        >
           Interpret your blood tests intelligently and receive personalized health recommendations.
-        </p>
-        <Link to="/Login"> {/* Utiliza Link para dirigir al usuario a la página de Login */}
+        </p> {/* Paragraph with custom styles */}
+        <Link to="/Login"> {/* Link component to navigate to the Login page */}
           <button
             style={{
               position: 'absolute',
@@ -29,19 +50,19 @@ const Home = () => {
               boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
             }}
             onMouseOver={(e) => {
-              e.target.style.backgroundColor = '#F05D5D';
+              e.target.style.backgroundColor = '#F05D5D';  // Change button background color on mouse over
             }}
             onMouseOut={(e) => {
-              e.target.style.backgroundColor = '#FF3131';
+              e.target.style.backgroundColor = '#FF3131';  // Revert button background color on mouse out
             }}
           >
             Get Started Now
-          </button>
+          </button> {/* Button with dynamic style and navigation functionality */}
         </Link>
       </div>
-      {/* Resto del contenido de tu página de inicio */}
+      {/* Additional content for your home page can be added here */}
     </>
   );
 };
 
-export default Home;
+export default Home;  // Export the Home component for use in other parts of the application
