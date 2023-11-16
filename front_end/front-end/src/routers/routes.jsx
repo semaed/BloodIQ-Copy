f9ed99accbd420f97b084703d8ba29aea1dcc7d5
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:52:50 -0400

    Update routes.jsx
    Adding comments to the code

--- front_end/front-end/src/routers/routes.jsx
+++ front_end/front-end/src/routers/routes.jsx
@@ -1,44 +1,46 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import React, { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-import Home from '../Pages/Home';
-import Login from '../Pages/Login';
-import CreateAccount from '../Pages/CreateAccount';
-import TestProvider from '../Pages/TestProvider';
-import Results from '../Pages/Results';
+import { Routes, Route, Navigate } from 'react-router-dom';  // Importing components from react-router-dom for routing
+import React, { useEffect } from 'react';  // Importing React and useEffect hook
+import { useNavigate } from 'react-router-dom';  // Importing useNavigate for programmatic navigation
+import { useAuth } from '../context/AuthContext';  // Importing the useAuth hook from AuthContext
+import Home from '../Pages/Home';  // Importing the Home component
+import Login from '../Pages/Login';  // Importing the Login component
+import CreateAccount from '../Pages/CreateAccount';  // Importing the CreateAccount component
+import TestProvider from '../Pages/TestProvider';  // Importing the TestProvider component
+import Results from '../Pages/Results';  // Importing the Results component
 
+// Component for protecting routes that require authentication
 const PrivateRoute = () => {
-  const { user } = useAuth();
-  const navigate = useNavigate();
+  const { user } = useAuth();  // Using the useAuth hook to access the current user
+  const navigate = useNavigate();  // Using useNavigate for navigation
 
   useEffect(() => {
     if (!user) {
-      // Redirige al usuario a la página de inicio de sesión si no está autenticado
+      // Redirecting the user to the Login page if not authenticated
       navigate('/Login');
     }
-  }, [user, navigate]);
+  }, [user, navigate]);  // Adding user and navigate to the dependency array
 
   if (!user) {
-    // Evita renderizar el componente si no está autenticado
+    // Returning a Navigate component to redirect unauthenticated users
     return <Navigate to="/Login" />;
   }
 
-  return <TestProvider />;
+  return <TestProvider />;  // Rendering TestProvider component for authenticated users
 };
 
+// Main component for defining application routes
 const AppRoutes = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/CreateAccount" element={<CreateAccount />} />
-        <Route path="/TestProvider" element={<PrivateRoute />} />
-        <Route path="/Results" element={<Results />} />
+      <Routes>  {/* Defining application routes */}
+        <Route path="/" element={<Home />} />  {/* Route for the home page */}
+        <Route path="/Login" element={<Login />} />  {/* Route for the login page */}
+        <Route path="/CreateAccount" element={<CreateAccount />} />  {/* Route for the create account page */}
+        <Route path="/TestProvider" element={<PrivateRoute />} />  {/* Private route for authenticated users */}
+        <Route path="/Results" element={<Results />} />  {/* Route for the results page */}
       </Routes>
     </div>
   );
 };
 
-export default AppRoutes;
+export default AppRoutes;  // Exporting AppRoutes for use in the application
