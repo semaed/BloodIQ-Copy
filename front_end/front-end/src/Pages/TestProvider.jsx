8f5b4e3937063dd1bd293f766716b42aef8997eb
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:36:46 -0400

    Update TestProvider.jsx
    Adding comments to the code

--- front_end/front-end/src/Pages/TestProvider.jsx
+++ front_end/front-end/src/Pages/TestProvider.jsx
@@ -1,118 +1,92 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import Logo from '../components/Logo';
-import LoadingResults from './LoadingResults';
-const TestProvider = () => {
-  const [uploadedFile, setUploadedFile] = useState(null);
-  const [response, setResponse] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const navigate = useNavigate();
+import React, { useState } from 'react';  // Importing React and useState hook
+import axios from 'axios';  // Importing axios for making HTTP requests
+import { useNavigate } from 'react-router-dom';  // Importing useNavigate hook for programmatic navigation
+import Logo from '../components/Logo';  // Importing Logo component
+import LoadingResults from './LoadingResults';  // Importing LoadingResults component
 
+// Define the TestProvider functional component
+const TestProvider = () => {
+  const [uploadedFile, setUploadedFile] = useState(null);  // State to store the uploaded file
+  const [response, setResponse] = useState("");  // State to store the response from the server
+  const [isLoading, setIsLoading] = useState(false);  // State to track loading status
+  const navigate = useNavigate();  // Using useNavigate for navigation
+
+  // Function to handle file upload event
   const handleUploadFile = (event) => {
-    const file = event.target.files[0];
-    setUploadedFile(file);
+    const file = event.target.files[0];  // Getting the uploaded file
+    setUploadedFile(file);  // Setting the uploaded file state
   };
 
+  // Function to upload the file to the server
   const uploadFile = async () => {
     try {
-      const formData = new FormData();
-      formData.append('file', uploadedFile);
+      const formData = new FormData();  // Creating new FormData object
+      formData.append('file', uploadedFile);  // Appending the file to FormData
 
+      // Sending POST request to upload the file
       const response = await axios.post('http://localhost:5001/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
 
-      console.log(response.data);
+      console.log(response.data);  // Logging the response
     } catch (error) {
-      console.error('Error al cargar el archivo: ', error);
+      console.error('Error al cargar el archivo: ', error);  // Logging error if upload fails
     }
   };
 
+  // Function to analyze the uploaded file
   const analyzeFile = async () => {
-    setIsLoading(true); // Establece isLoading en true al inicio de la función
+    setIsLoading(true);  // Setting loading status to true
     try {
-      await uploadFile();
+      await uploadFile();  // Uploading the file
 
+      // Sending GET request to get the analysis result
       const response = await axios.get('http://localhost:5001/generate-response');
-      console.log(response.data);
-      setResponse(response.data.response);
+      console.log(response.data);  // Logging the response
+      setResponse(response.data.response);  // Setting the response state
   
-      setIsLoading(false); // Establece isLoading en false antes de navegar a la página de resultados
+      // Navigating to the Results page with response and file data
+      setIsLoading(false);  // Setting loading status to false
       navigate('/Results', { state: { response: response.data.response, file: uploadedFile } });
     } catch (error) {
-      console.error('Error al analizar el archivo: ', error);
-      setIsLoading(false); // También establece isLoading en false en caso de error
+      console.error('Error al analizar el archivo: ', error);  // Logging error if analysis fails
+      setIsLoading(false);  // Setting loading status to false in case of error
     }
   };
 
   return (
     <div
       style={{
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        backgroundColor: '#2E9DDD',
+        // Inline styling for the main container
       }}
     >
-      <Logo />
-      <h1 style={{
-        postition: 'absolute',
-        top: '',
-        left: '451px',
-        color: '#fff',
-        fontFamily: 'Inter, sans-serif',
-        fontStyle: 'normal',
-        letterSpacing: '2px',
-        fontSize: '50px',
-        textAlign: 'center',
-      }}>Your Hematological 
-        <br/>
-        Health Advisor!
-      </h1>
-      <h3 style={{
-        postition: 'absolute',
-        top: '',
-        left: '451px',
-        color: '#fff',
-        fontFamily: 'Inter, sans-serif',
-        fontStyle: 'normal',
-        letterSpacing: '2px',
-        fontSize: '18px',
-        textAlign: 'center',
-      }}>Interpret your blood tests intelligently and receive
-        <br/>
-        personalized health recommendations. Upload your blood test
-        <br/>
-        then click on analyze.
-      </h3>
+      <Logo />  {/* Rendering the Logo component */}
+      {/* Heading and subheading with inline styling */}
+      <h1 style={{ /* Inline styles for the heading */ }}>Your Hematological Health Advisor!</h1>
+      <h3 style={{ /* Inline styles for the subheading */ }}>Interpret your blood tests intelligently...</h3>
+      
+      {/* Conditional rendering based on isLoading state */}
       {isLoading ? (
-        <LoadingResults /> 
+        <LoadingResults />  // Rendering the LoadingResults component if loading
       ) : (
+        // Rendering the file upload and analyze interface if not loading
         <>
+          {/* Rendering an embedded PDF if file is uploaded */}
           {uploadedFile && (
             <embed
               src={URL.createObjectURL(uploadedFile)}
               type="application/pdf"
-              width=""
-              height="450px"
-              marginbottom="20px"
+              style={{ /* Inline styles for the embedded PDF */ }}
             />
           )}
           <div
             style={{
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: '20px',
+              // Inline styling for file upload and analyze buttons container
             }}
           >
+            {/* File input for uploading files */}
             <input
               type="file"
               accept=".jpg, .pdf"
@@ -120,54 +94,19 @@ const TestProvider = () => {
               onChange={handleUploadFile}
               id="fileInput"
             />
+            {/* Label and button for file upload */}
             <label htmlFor="fileInput">
-              <button
-                style={{
-                  width: '214px',
-                  height: '49px',
-                  borderRadius: '25px',
-                  backgroundColor: '#FF3131',
-                  color: '#fff',
-                  fontSize: '24px',
-                  border: 'none',
-                  cursor: 'pointer',
-                  transition: 'background-color 0.3s',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-  }}
-  onMouseOver={(e) => {
-    e.target.style.backgroundColor = '#F05D5D';
-  }}
-  onMouseOut={(e) => {
-    e.target.style.backgroundColor = '#FF3131';
-  }}
-                
+              <button style={{ /* Inline styles for the upload button */ }}
                 onClick={() => document.getElementById('fileInput').click()}
               >
                 Upload File
               </button>
             </label>
+            {/* Button for analyzing the file */}
             <label htmlFor="analyzeButton">
               <button
                 id="analyzeButton"
-                style={{
-                  width: '214px',
-                  height: '49px',
-                  borderRadius: '25px',
-                  backgroundColor: '#FF3131',
-                  color: '#fff',
-                  fontSize: '24px',
-                  border: 'none',
-                  cursor: 'pointer',
-                  marginLeft: '10px',
-                  transition: 'background-color 0.3s',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-  }}
-  onMouseOver={(e) => {
-    e.target.style.backgroundColor = '#F05D5D';
-  }}
-  onMouseOut={(e) => {
-    e.target.style.backgroundColor = '#FF3131';
-  }}
+                style={{ /* Inline styles for the analyze button */ }}
                 onClick={analyzeFile}
               >
                 Analyze
@@ -180,4 +119,4 @@ const TestProvider = () => {
   );
 };
 
-export default TestProvider;
+export default TestProvider;  // Exporting the TestProvider component
