6752cc8e4fea807a69160506d27cdfe11bfc380d
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:21:31 -0400

    Update CreateAccount.jsx
    Adding comments to the code

--- front_end/front-end/src/Pages/CreateAccount.jsx
+++ front_end/front-end/src/Pages/CreateAccount.jsx
@@ -1,24 +1,25 @@
-// CreateAccount.jsx
-import Logo from '../components/Logo';
-import React from 'react';
-import "../App.css"
-import RegistrationForm from '../components/Forms/RegistrationForm'; 
+// Importing the necessary modules and components
+import Logo from '../components/Logo';  // Importing the Logo component from the components directory
+import React from 'react';  // Importing React to use JSX and create the component
+import "../App.css"  // Importing the main CSS file for styling
+import RegistrationForm from '../components/Forms/RegistrationForm';  // Importing the RegistrationForm component
 
+// Defining custom styles for the form container
 const formContainerStyles = {
-  display: 'flex',
-  justifyContent: 'center', 
+  display: 'flex',  // Using flexbox for layout
+  justifyContent: 'center',  // Centering content horizontally in the flex container
 };
 
+// Define the CreateAccount functional component
 function CreateAccount() {
   return (
-    <div id="rectangle">
-      <Logo />
-      <div style={formContainerStyles}>
-        <RegistrationForm />
+    <div id="rectangle">  {/* Main container div with an id of "rectangle" */}
+      <Logo />  {/* Rendering the Logo component */}
+      <div style={formContainerStyles}>  {/* Applying the custom styles to the form container */}
+        <RegistrationForm />  {/* Rendering the RegistrationForm component */}
       </div>
-
     </div>
   );
 }
 
-export default CreateAccount;
+export default CreateAccount;  // Exporting the CreateAccount component for use in other parts of the app
