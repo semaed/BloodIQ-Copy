64d7b3ed7773d03cf4db43096080e7fb42d2f75f
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:54:39 -0400

    Update App.jsx
    Adding comments to the code

--- front_end/front-end/src/App.jsx
+++ front_end/front-end/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import {AppRoutes} from './routers/routes';
-import {LoadingResults} from './Pages/LoadingResults';
+import React from 'react';  // Importing React to create a functional component
+import { AppRoutes } from './routers/routes';  // Importing AppRoutes for handling routing in the app
+import { LoadingResults } from './Pages/LoadingResults';  // Importing LoadingResults, though it's not used in this component
+
+// Define the App functional component
 function App() {
   return (
-    <div className="App">
-      <AppRoutes />
+    <div className="App">  {/* Container div with className 'App' */}
+      <AppRoutes />  {/* Rendering the AppRoutes component for routing */}
     </div>
   );
 }
 
-export default App;
+export default App;  // Exporting App component for use in the root index.js file
