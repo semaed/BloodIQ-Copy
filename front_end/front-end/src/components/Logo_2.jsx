333daafffa996591ae9c40231d31563475a330ae
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:47:06 -0400

    Update Logo_2.jsx
    Adding comments to the code

--- front_end/front-end/src/components/Logo_2.jsx
+++ front_end/front-end/src/components/Logo_2.jsx
@@ -1,15 +1,16 @@
-// Logo.jsx
-import React from 'react';
-import styled from 'styled-components';
-import logo from '../assets/logo_2.svg'; // Asegúrate de que la ruta sea correcta
+import React from 'react';  // Importing React for creating the component
+import styled from 'styled-components';  // Importing styled-components for custom styling
+import logo from '../assets/logo_2.svg'; // Importing the logo image, ensure the path is correct
 
+// Creating a styled image component using styled-components
 const StyledLogo = styled.img`
-  position: absolute;
-  top: 50px;
-  width: 704px;
-  height: 160px;
+  position: absolute;  // Setting the position of the logo to absolute
+  top: 50px;  // Positioning the logo 50 pixels from the top of its parent
+  width: 704px;  // Setting the width of the logo to 704 pixels
+  height: 160px;  // Setting the height of the logo to 160 pixels
 `;
 
-const Logo = () => <StyledLogo src={logo} alt="Logo" />;
+// Functional component for Logo
+const Logo = () => <StyledLogo src={logo} alt="Logo" />;  // Rendering the styled image with the logo
 
-export default Logo;
+export default Logo;  // Exporting Logo for use in other parts of the application
