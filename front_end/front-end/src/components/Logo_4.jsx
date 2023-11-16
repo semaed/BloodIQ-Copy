5958250a0374e9fa085c95ee8f00d91287ea69b1
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:49:25 -0400

    Update Logo_4.jsx
    Adding comments to the code

--- front_end/front-end/src/components/Logo_4.jsx
+++ front_end/front-end/src/components/Logo_4.jsx
@@ -1,15 +1,16 @@
-// Logo.jsx
-import React from 'react';
-import styled from 'styled-components';
-import logo from '../assets/logo_2.svg';
+import React from 'react';  // Importing React for creating the component
+import styled from 'styled-components';  // Importing styled-components for custom styling
+import logo from '../assets/logo_2.svg';  // Importing the logo image from assets
 
+// Creating a styled image component using styled-components
 const StyledLogo = styled.img`
-  top: 50px;
-  left: 63px;
-  width: 396px;
-  height: 90px;
+  top: 50px;  // Positioning the logo 50 pixels from the top of its parent
+  left: 63px;  // Positioning the logo 63 pixels from the left of its parent
+  width: 396px;  // Setting the width of the logo to 396 pixels
+  height: 90px;  // Setting the height of the logo to 90 pixels
 `;
 
-const Logo = () => <StyledLogo src={logo} alt="Logo" />;
+// Functional component for Logo
+const Logo = () => <StyledLogo src={logo} alt="Logo" />;  // Rendering the styled image with the logo
 
-export default Logo;
+export default Logo;  // Exporting Logo for use in other parts of the application
