03f5f26db1269a4597b9fa5313464f2435491f0a
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-11-15 20:31:28 -0400

    Update Login.jsx
    Adding comments to the code

--- front_end/front-end/src/Pages/Login.jsx
+++ front_end/front-end/src/Pages/Login.jsx
@@ -1,17 +1,19 @@
-// Login.jsx
-import Logo from '../components/Logo';
-import React from 'react';
-import "../App.css"
-import LoginForm from '../components/Forms/LoginForm'; 
+// Importing necessary libraries and components
+import Logo from '../components/Logo';  // Importing the Logo component from the components directory
+import React from 'react';  // Importing React to use JSX and create the component
+import "../App.css"  // Importing the main CSS file for styling
+import LoginForm from '../components/Forms/LoginForm';  // Importing the LoginForm component
 
+// Define the Login functional component
 function Login() {
   return (
+    // React Fragment to return multiple elements
     <>
-      <div id="rectangle"></div>
-      <Logo />
-      <LoginForm />
+      <div id="rectangle"></div>  {/* Empty div with an id of "rectangle", can be used for styling purposes */}
+      <Logo />  {/* Rendering the Logo component */}
+      <LoginForm />  {/* Rendering the LoginForm component */}
     </>
   );
 }
 
-export default Login;
+export default Login;  // Exporting the Login component for use in other parts of the app
